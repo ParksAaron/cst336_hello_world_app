57623f866db9a7b5f63951f99fdc32570350db74
Aaron Parks <aaparks@csumb.edu>
2018-09-09 18:03:34 +0000

    added git precedence testing

---
+++ examples/css/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Style Example</title>
+        <link href="./sheet1.css" rel="stylesheet" />
+    </head>
+    <body>
+        <style>
+        /* All paragraphs in the document */
+            p{
+                margin: 10px;
+            }
+        </style>
+        <p id = first style = "background-color: orange"; >
+          Poopy-di scoop Scoop-diddy-whoop Whoop-di-scoop-di-poop Poop-di-scoopty Scoopty-whoop Whoopity-scoop, whoop-poop Poop-diddy, whoop-scoop Poop, poop Scoop-diddy-whoopp
+        </p>
+        <p class = "not highlighted">
+           Poopy-di scoop Scoop-diddy-whoop Whoop-di-scoop-di-poop Poop-di-scoopty Scoopty-whoop Whoopity-scoop, whoop-poop Poop-diddy, whoop-scoop Poop, poop Scoop-diddy-whoopp        
+        </p>
+        <img style="width:1000px;" src="https://media.pitchfork.com/photos/5b92586a11c6c82d946abb24/2:1/w_790/kanye%20lil%20pump%20video%20i%20love%20it.png">
+        <style>
+            img {
+                height:200px;
+            }
+        </style>
+    </body>
+</html>
