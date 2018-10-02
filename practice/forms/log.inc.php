1ca317cf48bbda36cfd86662cc06a3f94b5359a2
Aaron Parks <aaparks@csumb.edu>
2018-10-02 19:24:26 +0000

    lab4 finished

---
+++ practice/forms/log.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+// Uncomment if you want to use logging...need to install composer
+// See examples/logging/index.php for more on server side logging with composer
+
+
+echo '<p>$_POST: ';
+echo '<pre>';
+var_dump($_POST);
+echo '</pre>';
+echo "</p>";
+
+echo '<p>$_GET: ';
+echo '<pre>';
+var_dump($_GET);
+echo '</pre>';
+echo "</p>";
+
+
+?>
+
---
+++ practice/p4/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Practice 4</title>
+    </head>
+    <body>
+        <h1>Aces vs Kings</h1>
+        <form>
+            <input type="text" name=""/>
+            <input type="submit" value="Submit"/>
+        </form>
+
+    </body>
+</html>
