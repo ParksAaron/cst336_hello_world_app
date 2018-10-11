2b5e54d35bd554b4466fe695e1993933aee4ca97
Aaron Parks <aaparks@csumb.edu>
2018-10-11 06:36:21 +0000

    added updated lab5

--- labs/5/index.php
+++ labs/5/index.php
@@ -1,3 +1,48 @@
+<?php
+    include 'functions.php';
+    
+    
+    session_start();
+    
+    
+    if(!isset($_SESSION['cart'])){
+        $_SESSION['cart'] = array();
+    }
+    
+    
+    if(isset($_GET['query'])){
+        
+        include 'wmapi.php';
+        $items = getProducts($_GET['query']);
+    }
+
+
+    if(isset($_POST['itemName'])){
+        
+        $newItem = array();
+        $newItem['name'] = $_POST['itemName'];
+        $newItem['price'] = $_POST['itemPrice'];
+        $newItem['img'] = $_POST['itemImage'];
+        $newItem['id'] = $_POST['itemId'];
+        
+        
+        
+        
+        foreach($_SESSION['cart'] as &$item){
+            if($newItem['id'] == $item['id']){
+                $item['quantity'] += 1;
+                $found = true;
+            }
+        }
+        
+        
+        if($found != true){
+            $newItem['quantity'] = 1;
+            array_push($_SESSION['cart'], $newItem);
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -21,7 +66,9 @@
                     </div>
                     <ul class='nav navbar-nav'>
                         <li><a href='index.php'>Home</a></li>
-                        <li><a href='scart.php'>Cart</a></li>
+                        <li><a href='scart.php'>
+                        <span class='glyphicon glyphicon-shopping-cart' aria-hidden= 'true'>
+                        </span> Cart: <?php displayCartCount(); ?> </a></li>
                     </ul>
                 </div>
             </nav>
@@ -38,6 +85,9 @@
             </form>
             
             <!-- Display Search Results -->
+            <?php
+                displayResults(); 
+            ?>
             
         </div>
     </div>
--- labs/meme_lab/meme.php
+++ labs/meme_lab/meme.php
@@ -4,11 +4,28 @@ session_start();
 $line1 = $_POST['line1'];
 $line2 = $_POST['line2'];
 
-function createMeme($line1, $line2) {
+function createMeme($line1, $line2, $memeType) {
+  
+    if($memeType == 'college-grad'){
+      $memeURL = 'https://upload.wikimedia.org/wikipedia/commons/c/ca/LinusPaulingGraduation1922.jpg';
+    }
+    
+    elseif($memeType == 'thinking-ape'){
+      $memeURL = 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Deep_in_thought.jpg';
+    }
+    
+    elseif($memeType == 'coding'){
+      $memeURL = 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Typing_computer_screen_reflection.jpg';
+    }
+    
+    elseif($memeType == 'old-class'){
+      $memeURL = 'https://upload.wikimedia.org/wikipedia/commons/4/47/StateLibQld_1_100348.jpg';
+
+    }
     
     $dbConn = getDatabaseConnection(); 
 
-    $sql = "INSERT INTO `all_memes` (`id`, `line1`, `line2`) VALUES (NULL, '$line1', '$line2');"; 
+    $sql = "INSERT INTO `all_memes` (`id`, `line1`, `line2`, 'meme-type', 'meme_url) VALUES (NULL, '$line1', '$line2', '$memeType', '$memeURL');";
     $statement = $dbConn->prepare($sql); 
     
     $statement->execute(); 
@@ -25,13 +42,16 @@ function displayMemes() {
     $records = $statement->fetchAll(); 
     
     foreach ($records as $record) {
-        echo $record["line1"]."<br>"; 
-        echo $record["line2"]."<br>"; 
+      $memeURL = $record['meme_url'];
+      echo "<div class='meme-div' style='background-image:url(" . $memeURL . ");'>";
+      echo "<h2 class='line1'>" . $record['line1'] . " </h2>";
+      echo "<h2 class='line2'>" . $record['line2'] . " </h2>";
+      echo "</div>";
     }
 } 
 
 if (isset($_POST['line1']) && isset($_POST['line2'])) {
-  createMeme($_POST['line1'], $_POST['line2']); 
+  createMeme($_POST['line1'], $_POST['line2'], $_POST['meme-type']); 
 }
 
 ?>
@@ -41,12 +61,13 @@ if (isset($_POST['line1']) && isset($_POST['line2'])) {
   <head>
     <title>A Meme</title>
     <style>
-      #meme-div{
+      .meme-div{
       width: 450px;
       height: 450px;
       background-size: 100%;
       text-align: center;
       position: relative;
+      display:inline-block;
       }
       h2 {
         position: absolute;
@@ -58,22 +79,24 @@ if (isset($_POST['line1']) && isset($_POST['line2'])) {
         color: white;
         text-shadow: 1px 1px black;
       }
-      #line1 {
+      .line1 {
          top: 0;
        }
-      #line2 {
+      .line2 {
          bottom: 0;
        }
     </style>
   </head>
   <body>
+    <?php if(isset($_POST['line1']) && isset($_POST['line2'])) { ?>
     <h1>Your Meme</h1>
     <!--The image needs to be rendered for each new meme
     so set the div's background-image property inline -->
-    <div id="meme-div" style="background-image:url(https://upload.wikimedia.org/wikipedia/commons/f/ff/Deep_in_thought.jpg);">
-      <h2 id="line1"><?php echo $line1 ?></h2>
-      <h2 id="line2"><?php echo $line2 ?></h2>
+    <div class="meme-div" style="background-image:url(https://upload.wikimedia.org/wikipedia/commons/f/ff/Deep_in_thought.jpg);">
+      <h2 class="line1"><?php echo $line1 ?></h2>
+      <h2 class="line2"><?php echo $line2 ?></h2>
     </div>
+    <?php } ?>
     <h1>All Memes</h1>
     <?php displayMemes(); ?>
   </body>
